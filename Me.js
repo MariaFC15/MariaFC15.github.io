2d485deb216882d559f31bd8c94b4fdd70b230a6
Maria <mc8780@pleasantonusd.net>
2018-07-03 15:19:16 -0700

    initial

---
+++ Me.js
@@ -0,0 +1,10 @@
+alert("Welcome to my page!")
+<!DOCTYPE html>
+<html>
+<body>
+
+<button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button>
+
+<p id="demo"></p>
+
+</body>
